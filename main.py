a0cc541ca7b259602eab0b9bbbd0e7bef0006059
maxy8ken <156901573+maxy8ken@users.noreply.github.com>
2025-07-03 15:58:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from Employee import *
+from Manager import *
+from Shift import *
+from Roster import *
+from Object import *
+from Person import *
 
+
+while True:
+    print("1: Create new roster")
+    print("2: Create new employee")
+    print("3: Create new manager")
+    print("4: Create new shift")
+    print("5: Assign employee to shift")
+    input = int(input("What would you like to do? "))
+    if input == 1:
+        break
+    if input == 2:
+        break
+    if input == 3:
+        break
+    if input == 4:
+        break
+    if input == 5:
+        break
